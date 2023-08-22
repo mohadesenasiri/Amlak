de575d12dabd82b3b06cda23cb467f21e8ca7dfc
mohadese nasiri <nasiri.mohadese@gmail.com>
2023-08-23 00:44:48 +0330

    upload first change

---
+++ js/index.js
@@ -0,0 +1,43 @@
+jQuery('.prop-cats-slider').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
+
+jQuery('.blog-slider').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
